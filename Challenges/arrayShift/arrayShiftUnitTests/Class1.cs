9533585e2e90b324b2ad92944fbafed57c72d832
rebeccahong <rebeccayhong@gmail.com>
2018-10-09 16:27:30 -0700

    Add InlineData to test

--- Challenges/arrayShift/arrayShiftUnitTests/Class1.cs
+++ Challenges/arrayShift/arrayShiftUnitTests/Class1.cs
@@ -6,6 +6,8 @@ namespace ArrayShiftUnitTests
 {
     public class Class1
     {
+        [Theory]
+        [InlineData(new int[] {1, 2, 4, 5}, 3)]
         public void CanInsertValue()
         {
             // 3 parts of a unit test:
@@ -13,7 +15,7 @@ namespace ArrayShiftUnitTests
             // act
             // assert
 
-            Assert.Equal(new int[] {1, 2, 3, 4, 5}, ArrayShift(new int[] {1, 2, 4, 5}, 3));
+            Assert.Equal(new int[] {1, 2, 3, 4, 5}, ArrayShift(input, val));
         }
     }
 }
