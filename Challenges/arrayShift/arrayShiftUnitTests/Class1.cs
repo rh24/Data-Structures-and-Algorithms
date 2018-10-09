5fd88a2be8df0b9cc2ab24e3203d0cb744168727
rebeccahong <rebeccayhong@gmail.com>
2018-10-09 16:50:58 -0700

    Add second unit test

--- Challenges/arrayShift/arrayShiftUnitTests/Class1.cs
+++ Challenges/arrayShift/arrayShiftUnitTests/Class1.cs
@@ -8,6 +8,7 @@ namespace ArrayShiftUnitTests
     {
         [Theory]
         [InlineData(new int[] {1, 2, 3, 4, 5}, new int[] {1, 2, 4, 5}, 3)]
+        [InlineData(new int[] {10, 12, 15, 20, 77, 88}, new int[] {10, 12, 15, 77, 88}, 20)]
         public void CanInsertValue(int[] expected, int[] input, int val)
         {
             // 3 parts of a unit test:
