641c2d81402819392190922e31214405e909a451
rebeccahong <rebeccayhong@gmail.com>
2018-10-09 16:44:58 -0700

    Pass in expected output in inline data

--- Challenges/arrayShift/arrayShiftUnitTests/Class1.cs
+++ Challenges/arrayShift/arrayShiftUnitTests/Class1.cs
@@ -7,15 +7,15 @@ namespace ArrayShiftUnitTests
     public class Class1
     {
         [Theory]
-        [InlineData(new int[] {1, 2, 4, 5}, 3)]
-        public void CanInsertValue()
+        [InlineData(new int[] {1, 2, 3, 4, 5}, new int[] {1, 2, 4, 5}, 3)]
+        public void CanInsertValue(int[] expected, int[] input, int val)
         {
             // 3 parts of a unit test:
             // arrange
             // act
             // assert
 
-            Assert.Equal(new int[] {1, 2, 3, 4, 5}, ArrayShift(input, val));
+            Assert.Equal(expected, ArrayShift(input, val));
         }
     }
 }
