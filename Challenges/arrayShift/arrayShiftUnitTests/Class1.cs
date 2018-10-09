d45292c9188be5491066d6f645ff67e19f69c63f
rebeccahong <rebeccayhong@gmail.com>
2018-10-09 16:09:04 -0700

    Set up repo

---
+++ Challenges/arrayShift/arrayShiftUnitTests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using arrayShift;
+
+namespace ArrayShiftUnitTests
+{
+    public class Class1
+    {
+        public void CanInsertValue()
+        {
+            // 3 parts of a unit test:
+            // arrange
+            // act
+            // assert
+
+            Assert.Equal(new int[] {1, 2, 3, 4, 5}, Program.ArrayShift(new int[] {1, 2, 4, 5}, 3));
+        }
+    }
+}
