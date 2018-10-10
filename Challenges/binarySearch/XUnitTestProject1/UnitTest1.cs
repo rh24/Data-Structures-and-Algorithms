f0b16364a706a4e55c16b025f0d27c4b3b4447ce
rebeccahong <rebeccayhong@gmail.com>
2018-10-10 14:01:41 -0700

    Add two more tests

--- Challenges/binarySearch/XUnitTestProject1/UnitTest1.cs
+++ Challenges/binarySearch/XUnitTestProject1/UnitTest1.cs
@@ -17,6 +17,8 @@ namespace ArrayBinarySearchTests
 
         [Theory]
         [InlineData(new int[] { 1, 3, 6, 7 }, 3, 1)]
+        [InlineData(new int[] { 2 }, 2, 0)]
+        [InlineData(new int[] { 2 }, 5, -1)]
         public void Test1(int[] input, int target, int expected)
         {
             Assert.Equal(expected, BinarySearch(input, target));
