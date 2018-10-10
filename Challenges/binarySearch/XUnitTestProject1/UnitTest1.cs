cc3d7e09e637080b34030b5abd06e47c5c221066
rebeccahong <rebeccayhong@gmail.com>
2018-10-10 13:34:03 -0700

    Add xunit test project

---
+++ Challenges/binarySearch/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using static binarySearch.Program;
+
+namespace ArrayBinarySearchTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
--- Challenges/binarySearch/binarySearch/Program.cs
+++ Challenges/binarySearch/binarySearch/Program.cs
@@ -2,7 +2,7 @@
 
 namespace binarySearch
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
