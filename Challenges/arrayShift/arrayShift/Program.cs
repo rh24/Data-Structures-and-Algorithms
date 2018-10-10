9afcf4455670adfe420795561bffba43a0204a46
rebeccahong <rebeccayhong@gmail.com>
2018-10-09 17:39:57 -0700

    Add challenge solution to Program

--- Challenges/arrayShift/arrayShift/Program.cs
+++ Challenges/arrayShift/arrayShift/Program.cs
@@ -6,17 +6,22 @@ namespace arrayShift
     {
         static void Main(string[] args)
         {
-            ArrayShift();
+            //ArrayShift();
         }
 
         static int[] ArrayShift(int[] input, int val)
         {
             int[] newArr = new int[input.Length + 1];
+            int middle = input.Length % 2 == 0 ? newArr.Length / 2 : (newArr.Length / 2) + 1;
 
             for (int i = 0; i < newArr.Length; i++)
             {
-
+                if (i < middle) newArr[i] = input[i];
+                else if (i == middle) newArr[i] = val;
+                else newArr[i] = input[i - 1];
             }
+
+            return newArr;
         }
     }
 }
