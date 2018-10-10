1caa35183c33d13578b958d4d8aa2ee45c204019
rebeccahong <rebeccayhong@gmail.com>
2018-10-09 17:02:58 -0700

    Add third test

--- Challenges/arrayShift/arrayShiftUnitTests/Class1.cs
+++ Challenges/arrayShift/arrayShiftUnitTests/Class1.cs
@@ -9,6 +9,7 @@ namespace ArrayShiftUnitTests
         [Theory]
         [InlineData(new int[] {1, 2, 3, 4, 5}, new int[] {1, 2, 4, 5}, 3)]
         [InlineData(new int[] {10, 12, 15, 20, 77, 88}, new int[] {10, 12, 15, 77, 88}, 20)]
+        [InlineData(new int[] {4, 8, 15, 16, 23, 42}, new int[] {4, 8, 15, 23, 42}, 16)]
         public void CanInsertValue(int[] expected, int[] input, int val)
         {
             // 3 parts of a unit test:
