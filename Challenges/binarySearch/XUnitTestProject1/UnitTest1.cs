7c1bbddb6129d119fbcd0b88a8ce92aa3874b291
rebeccahong <rebeccayhong@gmail.com>
2018-10-11 09:23:20 -0700

    Delete zombie code

--- Challenges/binarySearch/XUnitTestProject1/UnitTest1.cs
+++ Challenges/binarySearch/XUnitTestProject1/UnitTest1.cs
@@ -6,15 +6,6 @@ namespace ArrayBinarySearchTests
 {
     public class UnitTest1
     {
-        /*
-        [Theory]
-        [InlineData(new int[] { 1, 3, 6, 7, 9, 15 }, 3, 1)]
-        public void Test1(int[] input, int target, int expected)
-        {
-            Assert.Equal(expected, BinarySearch(input, target));
-        }
-        */
-
         [Theory]
         [InlineData(new int[] { 1, 3, 6, 7 }, 3, 1)]
         [InlineData(new int[] { 1, 3, 6, 7, 8, 9 }, 9, 5)]
