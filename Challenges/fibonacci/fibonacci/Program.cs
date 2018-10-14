73fe03fce4a1b232a4bd4bcc3cf1be15f94d8d9e
rebeccahong <rebeccayhong@gmail.com>
2018-10-14 16:10:05 -0700

    Add solution

--- Challenges/fibonacci/fibonacci/Program.cs
+++ Challenges/fibonacci/fibonacci/Program.cs
@@ -6,7 +6,28 @@ namespace fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 30; i++)
+            {
+                Console.WriteLine(Fibonacci(i));
+            }
+            Console.ReadLine();
+        }
+
+        public static int Fibonacci(int n)
+        {
+            // starting numbers
+            int x = 0;
+            int y = 1;
+            int temp;
+
+            for (int i = 0; i < n; i++)
+            {
+                temp = x;
+                x = y;
+                y = temp + y;
+            }
+
+            return x;
         }
     }
 }
