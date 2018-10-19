6ada1a9b7729d407f748b7cae58221d464b4e3f9
rebeccahong <rebeccayhong@gmail.com>
2018-10-19 16:08:24 -0700

    Add queue class and methods

--- Data-Structures/StackAndQueue/StackAndQueue/Classes/Queue.cs
+++ Data-Structures/StackAndQueue/StackAndQueue/Classes/Queue.cs
@@ -4,7 +4,37 @@ using System.Text;
 
 namespace StackAndQueue.Classes
 {
-    class Queue
+    public class Queue
     {
+        public Node Front { get; set; }
+        public Node Rear { get; set; }
+
+        public Queue(Node node)
+        {
+            Front = node;
+            Rear = node;
+        }
+
+        public Node Peek()
+        {
+            return Front;
+        }
+
+        // Add node to end of queue
+        public void Enqueue(Node node)
+        {
+            Rear.Next = node;
+            Rear = node;
+        }
+
+        // Remove node from first in queue
+        // Set your pointers equal to null so the unused objects can be GC'ed.
+        public Node Dequeue(Node node)
+        {
+            Node pointer = Peek();
+            Front = Front.Next;
+            pointer.Next = null;
+            return pointer;
+        }
     }
 }
