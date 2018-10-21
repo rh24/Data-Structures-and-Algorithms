07b7839690a51af6459c01429763fb6aeff2d0e9
rebeccahong <rebeccayhong@gmail.com>
2018-10-21 14:39:17 -0700

    Add solution for reverse LL in place

--- Challenges/LLReverse/LLReverse/Program.cs
+++ Challenges/LLReverse/LLReverse/Program.cs
@@ -12,7 +12,27 @@ namespace LLReverse
 
         static void ReverseLLInPlace(LList ll)
         {
+            Node a;
+            Node b;
+            Node c;
 
+            ll.Current = ll.Head;
+
+            while (ll.Current.Next.Next != null)
+            {
+                a = ll.Current;
+                b = a.Next;
+                c = b.Next;
+
+                a = c;
+                a.Next = b;
+                b.Next = ll.Current;
+                ll.Current = c;
+            }
+
+            ll.Head = ll.Current;
+
+            return ll;
         }
     }
 }
