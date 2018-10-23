a8e5083eb22111316b3d84cf86c572953682728b
rebeccahong <rebeccayhong@gmail.com>
2018-10-23 13:48:01 -0700

    Add project to challenges

---
+++ Challenges/FifoAnimalShelter/FifoAnimalShelter/AnimalShelter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StackAndQueue.Classes;
+
+namespace FIFOAnimalShelter
+{
+    public class AnimalShelter
+    {
+        public Queue Cats { get; set; }
+        public Queue Dogs { get; set; }
+    }
+}
