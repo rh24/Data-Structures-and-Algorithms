c3fa2167e469e46d2397d0cdd1f66f48860f579e
rebeccahong <rebeccayhong@gmail.com>
2018-10-24 10:04:55 -0700

    Add all tests

--- Challenges/FifoAnimalShelter/FIFOAnimalShelterTests/UnitTest1.cs
+++ Challenges/FifoAnimalShelter/FIFOAnimalShelterTests/UnitTest1.cs
@@ -27,11 +27,65 @@ namespace FIFOAnimalShelterTests
         }
 
         /// <summary>
+        /// Test that the first "cat" to be dequeued's name is "Kitty" because kitty was first in line. Afterwards, test that a "dog" is succesfully dequeued.
+        /// </summary>
+        [Fact]
+        public void TestDequeueWithPreference()
+        {
+            Cat meow = new Cat("Meow");
+            Dog scout = new Dog("Scout");
+            Cat waffles = new Cat("Waffles");
+            shelter.Enqueue(meow);
+            shelter.Enqueue(scout);
+            shelter.Enqueue(waffles);
+
+            Animal kitty = shelter.Dequeue("cat");
+            int animalCount = shelter.PrintAllAnimalsInShelterAndCount();
+
+            Assert.Equal("Kitty", kitty.Name);
+            Assert.True(kitty.FirstInQueue);
+            Assert.True(kitty.FirstOfKind);
+            //Assert.Equal(3, animalCount);
+
+            Animal secondDequeued = shelter.Dequeue("dog");
+            int secondAnimalCount = shelter.PrintAllAnimalsInShelterAndCount();
+
+            Assert.Equal("Scout", secondDequeued.Name);
+            Assert.True(secondDequeued.FirstOfKind);
+            Assert.False(secondDequeued.FirstInQueue);
+            //Assert.Equal(2, secondAnimalCount);
+        }
+
+        /// <summary>
+        /// Test whether each time an amial is dequeued without a preference, it is always the first animal in the queue.
+        /// </summary>
+        [Fact]
+        public void TestDequeueWithoutPreference()
+        {
+            Cat meow = new Cat("Meow");
+            Dog scout = new Dog("Scout");
+            Cat waffles = new Cat("Waffles");
+            shelter.Enqueue(meow);
+            shelter.Enqueue(scout);
+            shelter.Enqueue(waffles);
+
+            Animal mysteryAnimal = shelter.Dequeue();
+            Assert.True(mysteryAnimal.FirstInQueue);
+
+            Animal secondMysteryAnimal = shelter.Dequeue();
+            Assert.True(secondMysteryAnimal.FirstInQueue);
+
+            Animal thirdMysteryAnimal = shelter.Dequeue();
+            Assert.True(thirdMysteryAnimal.FirstInQueue);
+        }
+
+        /// <summary>
         /// Test whether a cat gets successfully enqueued.
         /// </summary>
         [Fact]
         public void TestEnqueue()
         {
+            var shelter = FifoAnimalShelter();
             Cat meow = new Cat("Meow");
             Dog scout = new Dog("Scout");
             Cat waffles = new Cat("Waffles");
@@ -45,32 +99,6 @@ namespace FIFOAnimalShelterTests
             Assert.Equal(2, animalCount);
             Assert.Equal(3, secondAnimalCount);
             Assert.Equal(4, thirdAnimalCount);
-        }
-
-        /// <summary>
-        /// Test that the first "cat" to be dequeued's name is "Kitty" because kitty was first in line. Afterwards, test that a "dog" is succesfully dequeued.
-        /// </summary>
-        [Fact]
-        public void TestDequeue()
-        {
-            Cat meow = new Cat("Meow");
-            Dog scout = new Dog("Scout");
-            Cat waffles = new Cat("Waffles");
-            shelter.Enqueue(meow);
-            shelter.Enqueue(scout);
-            shelter.Enqueue(waffles);
-
-            Animal kitty = shelter.Dequeue("cat");
-
-            Assert.Equal("Kitty", kitty.Name);
-            Assert.True(kitty.FirstInQueue);
-            Assert.True(kitty.FirstOfKind);
-
-            Animal secondDequeued = shelter.Dequeue("dog");
-
-            Assert.Equal("Scout", secondDequeued.Name);
-            Assert.True(secondDequeued.FirstOfKind);
-            Assert.False(secondDequeued.FirstInQueue);
         }
     }
 }
