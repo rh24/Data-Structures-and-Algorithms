12c13b214be93780b5d4f6dd3a08fd70fd4ca0ca
rebeccahong <rebeccayhong@gmail.com>
2018-10-29 10:17:33 -0700

    Add properties to Node class

--- Data-Structures/Trees/Trees/Classes/Node.cs
+++ Data-Structures/Trees/Trees/Classes/Node.cs
@@ -6,5 +6,8 @@ namespace Trees.Classes
 {
     public class Node
     {
+        public Node Value { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
     }
 }
