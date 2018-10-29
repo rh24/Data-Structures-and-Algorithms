46800a72e2a63267c865f3400c5d5f936d7c0679
rebeccahong <rebeccayhong@gmail.com>
2018-10-29 11:51:33 -0700

    Try writing Add method in BST class

--- Data-Structures/Trees/Trees/Classes/BinarySearchTree.cs
+++ Data-Structures/Trees/Trees/Classes/BinarySearchTree.cs
@@ -6,8 +6,39 @@ namespace Trees.Classes
 {
     public class BinarySearchTree
     {
-        public bool Add(Node newNode, Node existingNode)
+        public bool Add(Node newNode, Node root)
         {
+            try
+            {
+                while (root != null)
+                {
+                    if ((int)newNode.Value > (int)root.LeftChild.Value)
+                    {
+                        root = root.RightChild;
+                        Add(newNode, root);
+                    }
+
+                    if ((int)newNode.Value < (int)root.RightChild.Value)
+                    {
+                        root = root.LeftChild;
+                        Add(newNode, root);
+                    }
+
+                    // you found a leaf!
+                    if (root.RightChild == null && root.LeftChild == null)
+                    {
+                        if ((int)root.Value < (int)newNode.Value) root.RightChild = newNode;
+                        else root.LeftChild = newNode;
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
 
         }
 
