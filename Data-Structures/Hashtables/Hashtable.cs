ce0fe6c9c5b2232985b7be2b3c5a2e4e34645bef
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-12 20:59:36 -0800

    Stub out preliminary add method

--- Data-Structures/Hashtables/Hashtable.cs
+++ Data-Structures/Hashtables/Hashtable.cs
@@ -1,11 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 public class Hashtable
 {
     // Use private set to grow size of Hashtable
-    public int Buckets { get; private set; } = 1024;
+    public int Buckets { get; set; } = 1024;
+    public int PrimeMultiplier { get; set; } = 599;
+    public LinkedListNode[] HT { get; set; } = new LinkedListNode[Buckets];
 
+    // Default constructor
     public Hashtable()
     {
+    }
+
+    // Add
+    public bool Add(string key, object value)
+    {
+        int asciiVal = 0;
+
+        for (int i = 0; i < key.Length; i++)
+        {
+            asciiVal += Encoding.ASCII.GetBytes(key[i]);
+        }
+
+        HT[(asciiVal * PrimeMultiplier) % Buckets] = value;
     }
 }
