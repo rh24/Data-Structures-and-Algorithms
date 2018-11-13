3ef6f3e33e2bcb82a7ee37bdc48eb1fe4fdb74c7
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-12 22:56:06 -0800

    Update Main method in Program class

--- Data-Structures/Hashtables/Hashtables/Program.cs
+++ Data-Structures/Hashtables/Hashtables/Program.cs
@@ -8,7 +8,9 @@ namespace Hashtables
         {
             Hashtable ht = new Hashtable();
             Console.WriteLine($"ht Buckets: {ht.HT.Length}");
+            Console.WriteLine("Adding key: 'wen', value: 'turtle'");
             Console.WriteLine(ht.Add("wen", "turtle"));
+            Console.WriteLine("Find value of 'wen' by looking up key in hashtable:");
             Console.WriteLine(ht.Find("wen"));
         }
     }
