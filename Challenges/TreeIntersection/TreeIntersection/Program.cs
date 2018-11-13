5b07458340bc692e5cefe9c6989d9de7f97b009a
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-13 14:11:59 -0800

    Add tree intersections project and dependencies

---
+++ Challenges/TreeIntersection/TreeIntersection/Program.cs
@@ -0,0 +1,20 @@
+﻿using Hashtables;
+using System;
+using System.Collections.Generic;
+using Trees.Classes;
+
+namespace TreeIntersection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static List<Node> TreeIntersections(BinaryTree bt1, BinaryTree bt2)
+        {
+
+        }
+    }
+}
