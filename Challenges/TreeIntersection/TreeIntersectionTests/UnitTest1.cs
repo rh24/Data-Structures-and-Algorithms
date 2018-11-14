382d631aac1de866b3ef31418f872f9b6a31b370
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-13 16:55:56 -0800

    Add empty unit tests project

---
+++ Challenges/TreeIntersection/TreeIntersectionTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Trees.Classes;
+
+namespace TreeIntersectionTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
