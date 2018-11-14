058b82e8ca75e932411597461710057cecdbd6ee
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-13 17:15:57 -0800

    Add helper method to traverse binary trees

--- Challenges/TreeIntersection/TreeIntersection/Program.cs
+++ Challenges/TreeIntersection/TreeIntersection/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Hashtables;
 using System;
-using System.Collections.Generic;
+using System.Collections.Generic.List;
 using Trees.Classes;
 
 namespace TreeIntersection
@@ -12,9 +12,21 @@ namespace TreeIntersection
             Console.WriteLine("Hello World!");
         }
 
-        static List<Node> TreeIntersections(BinaryTree bt1, BinaryTree bt2)
+        public static List<Node> TreeIntersections(BinaryTree bt1, BinaryTree bt2,
         {
+            Hashtable ht1 = new Hashtable();
+            Hashtable ht2 = new Hashtable();
 
+            ht1 = RecursivePreOrder(bt1.Root, ht1);
+            ht2 = RecursivePreOrder(bt2.Root, ht2);
+        }
+         
+        public static Hashtable RecursivePreOrder(Trees.Classes.Node node, Hashtable ht)
+        {
+            if (node == null) return ht;
+            ht.Add((string)node.Value, node.Value);
+            RecursivePreOrder(node.LeftChild, ht);
+            RecursivePreOrder(node.RightChild, ht);
         }
     }
 }
