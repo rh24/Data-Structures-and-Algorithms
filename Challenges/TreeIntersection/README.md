b508b47ae024f3c6d9fb441c24ba871b207539f0
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-14 13:50:52 -0800

    Finish solution and update README

--- Challenges/TreeIntersection/README.md
+++ Challenges/TreeIntersection/README.md
@@ -5,7 +5,7 @@ Given two binary trees as input, create a method called `TreeIntersections` that
 ## Big O
 
 * Space complexity: O(n). As the values in the binary tree inputs increase, the hashtable will increase in size.
-* Time complexity: O(n). A hashtable look up is O(1), but this solution traverses one entire hashtable and looks up the values in the second hashtable.
+* Time complexity: O(n^2). A hashtable look up is O(1), but this solution traverses one entire hashtable and looks up the values in the second hashtable. Some of those linked lists in the buckets may have collisions and require additional looping. Therefore, the worst case is O(n^2).
 
 ## Whiteboard Solution
 
--- Challenges/TreeIntersection/TreeIntersection/Program.cs
+++ Challenges/TreeIntersection/TreeIntersection/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Hashtables;
 using System;
+using System.Collections.Generic;
 using System.Collections.Generic.List;
 using Trees.Classes;
+using Node = Hashtables.Node;
 
 namespace TreeIntersection
 {
@@ -12,13 +14,31 @@ namespace TreeIntersection
             Console.WriteLine("Hello World!");
         }
 
-        public static List<Node> TreeIntersections(BinaryTree bt1, BinaryTree bt2,
+        public static List<Hashtables.Node> TreeIntersections(BinaryTree bt1, BinaryTree bt2)
         {
             Hashtable ht1 = new Hashtable();
             Hashtable ht2 = new Hashtable();
+            List<Hashtables.Node> overlappingValues = new List<Hashtables.Node>();
 
             ht1 = RecursivePreOrder(bt1.Root, ht1);
             ht2 = RecursivePreOrder(bt2.Root, ht2);
+
+            for (int i = 0; i < ht1.HT.Length; i++)
+            {
+                if (ht1.HT[i] != null)
+                {
+                    Node current = ht1.HT[i].Head;
+
+                    while (current != null)
+                    {
+                        Node found = ht2.HT[i].Find(current);
+                        if (found != null) overlappingValues.Add(found);
+                        current = current.Next;
+                    }
+                }
+            }
+
+            return overlappingValues;
         }
          
         public static Hashtable RecursivePreOrder(Trees.Classes.Node node, Hashtable ht)
