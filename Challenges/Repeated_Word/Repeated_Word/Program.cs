2a2bc94e96d559aa2b5bc5df85dea8d387e36ceb
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-14 23:29:33 -0800

    Add test strings in Main method

--- Challenges/Repeated_Word/Repeated_Word/Program.cs
+++ Challenges/Repeated_Word/Repeated_Word/Program.cs
@@ -7,7 +7,18 @@ namespace Repeated_Word
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Expected: null
+            string test1 = "acatateanappleandbecameacatapple";
+            // Expected: null
+            string test2 = ";;;;; ; ;; ;;;";
+            // Expected: "a"
+            string test3 = "a, cat, ate an apple and became a cat apple";
+            // Expected: "turtles"
+            string test4 = "turtles move slowly. they are turtles";
+            // Expected: "an"
+            string test5 = "an apple ate an apple and became an apple.";
+
+
         }
 
         public string RepeatedWord(string longWord)
@@ -34,7 +45,6 @@ namespace Repeated_Word
             // Just in case there's no punctuation at the end of the string, and we still need to check for/add the last word of longWord in hashtable:
             if (ht.Find(temp) != null) return temp;
             else return null;
-
         }
     }
 }
