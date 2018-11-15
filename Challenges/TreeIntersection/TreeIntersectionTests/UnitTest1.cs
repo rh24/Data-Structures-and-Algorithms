f026e0841cc64f94ad279fcc054076c9fa5f98e1
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-14 19:10:44 -0800

    Add remaining unit tests

--- Challenges/TreeIntersection/TreeIntersectionTests/UnitTest1.cs
+++ Challenges/TreeIntersection/TreeIntersectionTests/UnitTest1.cs
@@ -27,7 +27,7 @@ namespace TreeIntersectionTests
         */
         /// </summary>
         [Fact]
-        public void Test1()
+        public void TestDifferentValuesInTree()
         {
             Node node3 = new Node(3);
             Node node4 = new Node(4);
@@ -53,6 +53,98 @@ namespace TreeIntersectionTests
 
             List<object> overlapping = TreeIntersections(bt1, bt2);
             List<object> expectedValues = new List<object>() { 4, 2, 1, 5 };
+            Assert.Equal(overlapping, expectedValues);
+        }
+
+        /// <summary>
+        /* BT1 and BT2 have identical node values. Therefore, every single one of its values should be printed to the console except for 3 and 7.
+
+        BT1:
+                 4
+               /   \
+              2     5
+             / \
+            1   3
+
+            BT2:
+                 4
+               /   \
+              2     5
+             / \
+            1   3
+         
+        */
+        /// </summary>
+        [Fact]
+        public void TestSameValuesInTree()
+        {
+            Node node3 = new Node(3);
+            Node node4 = new Node(4);
+            BinaryTree bt1 = new BinaryTree(node4);
+            Node node1 = new Node(1);
+            Node node2 = new Node(2);
+            Node node5 = new Node(5);
+            node4.LeftChild = node2;
+            node4.RightChild = node5;
+            node2.LeftChild = node1;
+            node2.RightChild = node3;
+
+            Node bt2node3 = new Node(3);
+            Node bt2node4 = new Node(4);
+            BinaryTree bt2 = new BinaryTree(bt2node4);
+            Node bt2node1 = new Node(1);
+            Node bt2node2 = new Node(2);
+            Node bt2node5 = new Node(5);
+            bt2node4.LeftChild = bt2node2;
+            bt2node4.RightChild = bt2node5;
+            bt2node2.LeftChild = bt2node1;
+            bt2node2.RightChild = bt2node3;
+
+            List<object> overlapping = TreeIntersections(bt1, bt2);
+            List<object> expectedValues = new List<object>() { 4, 2, 1, 3, 5 };
+            Assert.Equal(overlapping, expectedValues);
+        }
+
+        /// <summary>
+        /* BT1 and BT2 have a differing number of values.
+
+        BT1:
+                 4
+               /   \
+              2     5
+
+            BT2:
+                 4
+               /   \
+              2     5
+             / \
+            1   3
+         
+        */
+        /// </summary>
+        [Fact]
+        public void TestDifferentSizeTrees()
+        {
+            Node node4 = new Node(4);
+            BinaryTree bt1 = new BinaryTree(node4);
+            Node node2 = new Node(2);
+            Node node5 = new Node(5);
+            node4.LeftChild = node2;
+            node4.RightChild = node5;
+
+            Node bt2node3 = new Node(3);
+            Node bt2node4 = new Node(4);
+            BinaryTree bt2 = new BinaryTree(bt2node4);
+            Node bt2node1 = new Node(1);
+            Node bt2node2 = new Node(2);
+            Node bt2node5 = new Node(5);
+            bt2node4.LeftChild = bt2node2;
+            bt2node4.RightChild = bt2node5;
+            bt2node2.LeftChild = bt2node1;
+            bt2node2.RightChild = bt2node3;
+
+            List<object> overlapping = TreeIntersections(bt1, bt2);
+            List<object> expectedValues = new List<object>() { 4, 2, 5 };
             Assert.Equal(overlapping, expectedValues);
         }
     }
