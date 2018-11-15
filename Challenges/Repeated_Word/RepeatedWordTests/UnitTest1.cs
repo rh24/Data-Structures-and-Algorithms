f8e2f3ad36433f00403fa2548daa8d0d3e159592
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-14 23:25:38 -0800

    Add rough draft of solution

--- Challenges/Repeated_Word/RepeatedWordTests/UnitTest1.cs
+++ Challenges/Repeated_Word/RepeatedWordTests/UnitTest1.cs
@@ -1,5 +1,4 @@
-using System;
-using Xunit;
+﻿using Xunit;
 
 namespace RepeatedWordTests
 {
--- Challenges/Repeated_Word/Repeated_Word/Program.cs
+++ Challenges/Repeated_Word/Repeated_Word/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using Hashtables;
+using System;
 
 namespace Repeated_Word
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public string RepeatedWord(string longWord)
+        {
+            Hashtable ht = new Hashtable();
+            string temp = "";
+            string letters = "abcdefghijklmnopqrstuvwrxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            for (int i = 0; i < longWord.Length; i++)
+            {
+                if (letters.Contains(longWord[i])) String.Concat(temp, longWord[i]);
+                else if (ht.Find(temp) == null)
+                {
+                    ht.Add(temp, 1);
+                    temp = "";
+                }
+                else
+                {
+                    //ht.Add(temp, 2);
+                    return temp;
+                }
+            }
+
+            // Just in case there's no punctuation at the end of the string, and we still need to check for/add the last word of longWord in hashtable:
+            if (ht.Find(temp) != null) return temp;
+            else return null;
+
         }
     }
 }
