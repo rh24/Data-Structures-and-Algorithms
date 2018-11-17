08cc0cf52223a0c1f167237f4ca955c9b2df73d4
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-16 20:13:01 -0800

    Add first implementation

--- Challenges/MostCommonWord_EC/MostCommonWord_EC/Program.cs
+++ Challenges/MostCommonWord_EC/MostCommonWord_EC/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Text;
 
 namespace MostCommonWord_EC
 {
@@ -7,6 +9,33 @@ namespace MostCommonWord_EC
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static string MostCommonWord(string book)
+        {
+            Hashtable ht = new Hashtable();
+            StringBuilder temp = new StringBuilder();
+            string letters = "abcdefghijklmnopqrstuvwyz";
+            string mostCommon = "";
+            int tempMax = 1;
+
+            for (int i = 0; i < book.Length; i++)
+            {
+                if (letters.Contains(book[i])) temp.Append(book[i]);
+                else
+                {
+                    if (mostCommon == "") mostCommon = temp.ToString();
+                    if (!ht.ContainsKey(temp)) ht.Add(temp.ToString(), 1);
+                    else
+                    {
+                        ht[key: temp.ToString()] = (int)ht[key: temp.ToString()] + 1;
+                        if ((int)ht[key: temp.ToString()] > tempMax) mostCommon = temp.ToString();
+                    }
+                }
+
+            }
+
+            return mostCommon;
         }
     }
 }
