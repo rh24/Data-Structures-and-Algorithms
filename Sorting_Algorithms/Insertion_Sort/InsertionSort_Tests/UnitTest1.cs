a986fbe3efb1c1517e900f25f5aebdea6f63d000
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-19 22:12:05 -0800

    Add three tests for InsertionSort method

--- Sorting_Algorithms/Insertion_Sort/InsertionSort_Tests/UnitTest1.cs
+++ Sorting_Algorithms/Insertion_Sort/InsertionSort_Tests/UnitTest1.cs
@@ -1,14 +1,17 @@
-using System;
-using Xunit;
-
+﻿using Xunit;
+using static Insertion_Sort.Program;
 namespace InsertionSort_Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(new int[] { 2, 8, 5, 3, 1 }, new int[] { 1, 2, 3, 5, 8 })]
+        [InlineData(new int[] { 21, 400, 0, 33, 10 }, new int[] { 0, 10, 21, 33, 400 })]
+        [InlineData(new int[] { 5, 0, 0, 10, 1 }, new int[] { 0, 0, 1, 5, 10 })]
+        public void Test1(int[] unsortedArr, int[] sortedArr)
         {
-
+            int[] result = InsertionSort(unsortedArr);
+            Assert.Equal(sortedArr, result);
         }
     }
 }
--- Sorting_Algorithms/Insertion_Sort/Insertion_Sort/Program.cs
+++ Sorting_Algorithms/Insertion_Sort/Insertion_Sort/Program.cs
@@ -2,7 +2,7 @@
 
 namespace Insertion_Sort
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
