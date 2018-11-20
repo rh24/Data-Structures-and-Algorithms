6ada988066bd90147e29a093bc3eeed0952369d1
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-19 21:50:37 -0800

    Add InsertionSort solution and print in Main method

--- Sorting_Algorithms/Insertion_Sort/Insertion_Sort/Program.cs
+++ Sorting_Algorithms/Insertion_Sort/Insertion_Sort/Program.cs
@@ -6,7 +6,32 @@ namespace Insertion_Sort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] unsorted = new int[] { 2, 8, 5, 3, 1 };
+
+            // Print original v. insertion sorted array:
+            Console.WriteLine("Original unsorted array:" + Environment.NewLine + string.Join(", ", unsorted));
+            Console.WriteLine("Insertion sorted array:" + Environment.NewLine + string.Join(", ", InsertionSort(unsorted)));
+        }
+
+        public static int[] InsertionSort(int[] myArr)
+        {
+            for (int i = 1; i < myArr.Length; i++)
+            {
+                int temp = myArr[i];
+                int j = i - 1;
+
+                while (j >= 0 && temp < myArr[j])
+                {
+                    myArr[j + 1] = myArr[j];
+                    myArr[j] = temp;
+                    j--;
+                }
+
+                // Comment in to see line by line sorting after each loop:
+                // Console.WriteLine(string.Join(", ", myArr));
+            }
+
+            return myArr;
         }
     }
 }
