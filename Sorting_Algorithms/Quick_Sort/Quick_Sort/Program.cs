f9ecbea1b2db598ab69ca196ad3efcc6133eb51e
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-20 21:52:06 -0800

    Add empty quick sort project

---
+++ Sorting_Algorithms/Quick_Sort/Quick_Sort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Quick_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
