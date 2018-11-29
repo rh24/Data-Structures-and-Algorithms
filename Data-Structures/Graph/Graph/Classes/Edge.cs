cef02634b868e6fa9c98f6cb86d2ad01b16cd803
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-28 21:44:46 -0800

    Finish graph implementation

--- Data-Structures/Graph/Graph/Classes/Edge.cs
+++ Data-Structures/Graph/Graph/Classes/Edge.cs
@@ -5,5 +5,12 @@
         public Vertex To { get; set; }
         public Vertex From { get; set; }
         public int Weight { get; set; }
+
+        public Edge(Vertex from, Vertex to, int weight)
+        {
+            To = to;
+            From = from;
+            Weight = weight;
+        }
     }
 }
--- Data-Structures/Graph/Graph/Classes/Graph.cs
+++ Data-Structures/Graph/Graph/Classes/Graph.cs
@@ -1,54 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Graph.Classes
 {
     public class Graph
     {
-        public List<Tuple<Vertex, string>> AdjacencyList { get; set; }
+        public Dictionary<Vertex, List<Vertex>> AdjacencyList { get; set; }
+        public List<Edge> Edges { get; set; }
 
+        /// <summary>
+        /// Instantiate and a dictionary and a list of edges and set the props of the Graph object.
+        /// </summary>
         public Graph()
         {
-            AdjacencyList = new List<Tuple<Vertex, string>>();
+            AdjacencyList = new Dictionary<Vertex, List<Vertex>>();
+            Edges = new List<Edge>();
         }
 
         /// <summary>
         /// This method adds an edge with a weight to the graph between two vertices.
         /// </summary>
-        /// <param name="parent"></param>
-        /// <param name="child"></param>
-        public void AddEdge(Tuple<Vertex, string> parent, Tuple<Vertex, string> child)
+        /// <param name="from">The starting vertex from which to add the edge</param>
+        /// <param name="to">The ending vertex to which the edge is added</param>
+        public void AddEdge(Vertex from, Vertex to, int weight)
         {
-            foreach (var item in AdjacencyList)
-            {
-                var node = item.Item1;
-                var edge = item.Item2;
-            }
+            Edges.Add(new Edge(from, to, weight));
+            AdjacencyList[from].Add(to);
         }
 
         /// <summary>
         /// This method adds a node to the graph, but it does not add an edge to the graph.
         /// </summary>
-        /// <param name="node"></param>
-        public void AddVertex(Node node)
+        /// <param name="vertex">Vertex to add</param>
+        public void AddNode(Vertex vertex)
         {
-            List<Tuple<Vertex, string>> newList = new List<Tuple<Vertex, string>>();
-            AdjacencyList.Add(new Tuple<Vertex, string>(node, node.Value));
+            AdjacencyList.Add(vertex, new List<Vertex>());
         }
 
-        public void GetNodes()
+        /// <summary>
+        /// This method returns all the vertices in the graph.
+        /// </summary>
+        /// <returns>List of vertices</returns>
+        public List<Vertex> GetNodes()
         {
-
+            return new List<Vertex>(AdjacencyList.Keys);
         }
 
-        public LinkedList<Tuple<Vertex, int>> GetNeighbors(Vertex node)
+        /// <summary>
+        /// This method gets all the neighbors of a vertex in the graph.
+        /// </summary>
+        /// <param name="vertex">Vertex whose neighbors will be returned</param>
+        /// <returns>List of vertices that are connected by an edge to the input vertex</returns>
+        public List<Vertex> GetNeighbors(Vertex vertex)
         {
-
+            return AdjacencyList[vertex];
         }
 
+        /// <summary>
+        /// This method returns the number of vertices in the graph.
+        /// </summary>
+        /// <returns></returns>
         public int Size()
         {
-
+            return AdjacencyList.Count;
         }
     }
 }
